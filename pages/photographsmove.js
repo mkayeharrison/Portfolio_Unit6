61f884c8f956f23bcf3147c0ffec98a1435b60f4
Megan Harrison <novagirl99@gmail.com>
2017-03-13 23:57:30 +0000

    I changed stuff

---
+++ pages/photographsmove.js
@@ -0,0 +1,20 @@
+$("#mood-1").click( function(){
+    $("#mood-1").append( "http://stream1.gifsoup.com/view/513948/fresh-prince-of-bel-air-o.gif");
+})
+
+$("#mood-2").click( function(){
+    $("#mood-2").append("http://i48.tinypic.com/2rnwy9x.jpg");
+})
+
+$("#mood-3").click( function(){
+    $("#mood-3").append( "http://cdn4.gurl.com/wp-content/uploads/2013/04/fresh-prince-of-bel-air-geoffrey-sleeping.gif");
+})
+$("#mood-4").click( function(){
+    $("#mood-4").append( "https://media1.popsugar-assets.com/files/thumbor/JbXneoSJ4Ab2HzZnzab4qbFAJCc/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/09/25/831/n/1922283/de62532db3e65d80_Fresh-Prince-Of-Bel-Air-the-fresh-prince-of-bel-air-13456178-307-230/i/Tina-Turner-Turn-Up-Front-Step.gif");
+})
+$("#mood-5").click( function(){
+    $("#mood-5").append( "http://cdn2.gurl.com/wp-content/uploads/2013/04/tumblr_mlbrjo2Mba1rtkgrco1_500.gif");
+})
+$("#mood-6").click( function(){
+    $("#mood-6").append("https://media1.popsugar-assets.com/files/thumbor/x5PBMiogd_Bpe0NP42UgSGDFOaY/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/09/25/831/n/1922283/6c2f61ac7d29a4e3_2mq47bm/i/I-Wanna-Rock-Right-Now-My-Name-I-Came-Get-Down-Drum-Solo-Dance.jpg");
+})
