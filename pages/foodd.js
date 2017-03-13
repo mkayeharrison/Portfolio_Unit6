f5e8b374ba9b6bbd2f109d87c890902244df043c
Megan Harrison <novagirl99@gmail.com>
2017-03-13 22:07:45 +0000

    I changed stuff

---
+++ pages/foodd.js
@@ -0,0 +1,11 @@
+$("#meal-1").click( function(){
+   $("#meal-1").append("Pasta Bravo");
+});
+
+$("#meal-2").click( function(){
+    $("#meal-2").append("Sweet and Sour Chicken");
+});
+
+$("#meal-3").click( function(){
+    $("#meal-3").append("Crepes");
+});
