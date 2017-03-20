fc93f437e73addba0e07ec2d8a5ebc3921b01a70
Megan Harrison <novagirl99@gmail.com>
2017-03-20 23:27:53 +0000

    finished somethings

--- pages/photographsmove.js
+++ pages/photographsmove.js
@@ -1,20 +1,17 @@
-$("#mood-1").click( function(){
-    $("#mood-1").append( "http://stream1.gifsoup.com/view/513948/fresh-prince-of-bel-air-o.gif");
-})
 
-$("#mood-2").click( function(){
-    $("#mood-2").append("http://i48.tinypic.com/2rnwy9x.jpg");
-})
+$("#mood-1").click(function(){
+    $("#mood-1").attr("src", "https://media.giphy.com/media/b5LTssxCLpvVe/giphy.gif");
+});
+$("#mood-2").click(function(){
+    $("#mood-2").attr("src", "https://media.giphy.com/media/Zn7rsVqBTPAly/giphy.gif");
+});
+$("#mood-3").click(function(){
+    $("#mood-3").attr("src", "https://media.giphy.com/media/i8Q2TfHNvAdfW/giphy.gif");
+});
+$("#mood-4").click(function(){
+    $("#mood-4").attr("src", "https://media.giphy.com/media/jeLdZuzcmIQgg/giphy.gif");
+});
 
-$("#mood-3").click( function(){
-    $("#mood-3").append( "http://cdn4.gurl.com/wp-content/uploads/2013/04/fresh-prince-of-bel-air-geoffrey-sleeping.gif");
-})
-$("#mood-4").click( function(){
-    $("#mood-4").append( "https://media1.popsugar-assets.com/files/thumbor/JbXneoSJ4Ab2HzZnzab4qbFAJCc/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/09/25/831/n/1922283/de62532db3e65d80_Fresh-Prince-Of-Bel-Air-the-fresh-prince-of-bel-air-13456178-307-230/i/Tina-Turner-Turn-Up-Front-Step.gif");
-})
-$("#mood-5").click( function(){
-    $("#mood-5").append( "http://cdn2.gurl.com/wp-content/uploads/2013/04/tumblr_mlbrjo2Mba1rtkgrco1_500.gif");
-})
-$("#mood-6").click( function(){
-    $("#mood-6").append("https://media1.popsugar-assets.com/files/thumbor/x5PBMiogd_Bpe0NP42UgSGDFOaY/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/09/25/831/n/1922283/6c2f61ac7d29a4e3_2mq47bm/i/I-Wanna-Rock-Right-Now-My-Name-I-Came-Get-Down-Drum-Solo-Dance.jpg");
-})
+$("#mood-5").click(function(){
+    $("#mood-5").attr("src", "https://media.giphy.com/media/bNZMY3GkWEFLq/giphy.gif");
+});
